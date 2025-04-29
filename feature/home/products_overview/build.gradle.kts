7dd68129f61593cba0149135e846d7d4589ded3b
Stefan Jovanovic <stefan.jovanavich@gmail.com>
2025-04-29 10:48:47 +0200

    - MainProductCard.kt added.

--- feature/home/products_overview/build.gradle.kts
+++ feature/home/products_overview/build.gradle.kts
@@ -26,6 +26,12 @@ kotlin {
     }
 
     sourceSets {
+        androidMain.dependencies {
+            implementation(libs.ktor.android.client)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.darwin.client)
+        }
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
@@ -38,6 +44,11 @@ kotlin {
 
             implementation(libs.koin.compose)
             implementation(libs.koin.compose.viewmodel)
+
+            implementation(libs.coil3)
+            implementation(libs.coil3.compose)
+            implementation(libs.coil3.compose.core)
+            implementation(libs.coil3.network.ktor)
 
             implementation(project(path = ":shared"))
             implementation(project(path = ":data"))
